08bc98dd873506d5ec8eb88dcb214c07517e791b
Clay Beabout <beaboutclayton@gmail.com>
2022-03-26 21:28:45 -0500

    more, adding logging

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :conversation
+
+  validates :author, presence: true
+
+  def time_and_sender
+    "#{author.email} | #{created_at.strftime("%k:%M:%S")}"
+  end
 end
